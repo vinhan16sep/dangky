8d127be5b389507f74ded0d7be72ccd837cc4f91
minhtruong1993 <http.mt.html@gmail.com>
2019-03-13 00:48:41 +0700

    add users action for member

--- application/controllers/member/Dashboard.php
+++ application/controllers/member/Dashboard.php
@@ -22,10 +22,27 @@ class Dashboard extends Member_Controller {
 
     	if($user->member_role == 'member'){
             $this->data['team'] = $this->get_personal_products($user->id);
-        }
-    	$this->data['user_id'] = $user->id;
+            $this->data['user_id'] = $user->id;
 
-        $this->render('member/dashboard_view');
+            $this->render('member/dashboard_view');
+        }elseif($user->member_role == 'leader'){
+            $this->data['team'] = $this->get_personal_products_by_leader($user->id);
+            $this->data['user_id'] = $user->id;
+
+            $this->render('member/dashboard_view');
+        }
+    	
+    }
+
+    public function users(){
+        $user = $this->ion_auth->user()->row();
+        if ($user->member_role == 'leader') {
+            $this->data['team'] = $this->get_personal_members_by_leader($user->id);
+
+            $this->data['user_id'] = $user->id;
+
+            $this->render('member/dashboard_leader_view');
+        }
     }
 
     public function get_personal_products($user_id){
@@ -42,6 +59,50 @@ class Dashboard extends Member_Controller {
                     if($product_ids){
                         $products = $this->information_model->get_personal_products($product_ids);
                         $list_team[$key]['product_list'] = $products;
+                    }
+
+                }
+            }
+        }
+        return $list_team;
+    }
+
+    public function get_personal_products_by_leader($user_id){
+        $list_team = $this->team_model->get_current_leader($user_id);
+        if ( !empty($list_team) ) {
+            foreach($list_team as $key => $value){
+                $product_ids = explode(',', $value['product_id']);
+                if ( is_array($product_ids) && !empty($product_ids) ) {
+                    foreach($product_ids as $k => $val){
+                        if(empty($val)){
+                            unset($product_ids[$k]);
+                        }
+                    }
+                    if($product_ids){
+                        $products = $this->information_model->get_personal_products($product_ids);
+                        $list_team[$key]['product_list'] = $products;
+                    }
+
+                }
+            }
+        }
+        return $list_team;
+    }
+
+    public function get_personal_members_by_leader($user_id){
+        $list_team = $this->team_model->get_current_leader($user_id);
+        if ( !empty($list_team) ) {
+            foreach($list_team as $key => $value){
+                $member_ids = explode(',', $value['member_id']);
+                if ( is_array($member_ids) && !empty($member_ids) ) {
+                    foreach($member_ids as $k => $val){
+                        if(empty($val)){
+                            unset($member_ids[$k]);
+                        }
+                    }
+                    if($member_ids){
+                        $members = $this->information_model->get_personal_members($member_ids);
+                        $list_team[$key]['member_list'] = $members;
                     }
 
                 }
